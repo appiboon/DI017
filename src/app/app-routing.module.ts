e521fc4a193536530c237593568c00e75df82610
appiboon <appiboon@gmail.com>
2018-01-04 10:36:19 +0530

    institute page are not run and institute-search is run
    Author:riya

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,23 @@ import { Routes, RouterModule } from '@angular/router';
 import {LoginComponent} from './login/login.component';
 import {InstituteComponent} from './institute/institute.component';
 import {InstituteSearchComponent} from './institute-search/institute-search.component';
+import {TrustComponent} from './trust/trust.component';
+import {TrusteeComponent} from './trustee/trustee.component';
+import {DesignationComponent} from './designation/designation.component';
+import {EmployeeComponent} from './employee/employee.component';
+import {EmployeeSearchComponent} from './employee-search/employee-search.component';
+import {StandardComponent} from './standard/standard.component';
+import {StandardSearchComponent} from './standard-search/standard-search.component';
+import {SubjectComponent} from './subject/subject.component';
+import {SubjectSearchComponent} from './subject-search/subject-search.component';
+import {StudentComponent} from './student/student.component';
+import {StudentSearchComponent} from './student-search/student-search.component';
+import {ExamComponent} from './exam/exam.component';
+import {ExamSearchComponent} from './exam-search/exam-search.component';
+import {FeesComponent} from './fees/fees.component';
+import {FeesSearchComponent} from './fees-search/fees-search.component';
+import {FeesSetupComponent} from './fees-setup/fees-setup.component';
+import {FeesHeadSearchComponent} from './fees-head-search/fees-head-search.component';
 import {AuthenticationGuard} from "./authentication.guard";
 import { HttpClientModule } from '@angular/common/http';
 import { HttpModule } from '@angular/http';
@@ -13,6 +30,24 @@ const appRoutes: Routes = [
   { path: 'login', component: LoginComponent},
   { path: 'institute', canActivate: [AuthenticationGuard], component: InstituteComponent},
   { path: 'institute_search', canActivate: [AuthenticationGuard], component: InstituteSearchComponent},
+  { path: 'trust', canActivate: [AuthenticationGuard], component: TrustComponent},
+  { path: 'trustee', canActivate: [AuthenticationGuard], component: TrusteeComponent},
+  { path: 'designation', canActivate: [AuthenticationGuard], component: DesignationComponent},
+  { path: 'employee', canActivate: [AuthenticationGuard], component: EmployeeComponent},
+  { path: 'employee_search', canActivate: [AuthenticationGuard], component: EmployeeSearchComponent},
+  { path: 'standard', canActivate: [AuthenticationGuard], component: StandardComponent},
+  { path: 'standard_search', canActivate: [AuthenticationGuard], component: StandardSearchComponent},
+  { path: 'subject', canActivate: [AuthenticationGuard], component: SubjectComponent},
+  { path: 'subject_search', canActivate: [AuthenticationGuard], component: SubjectSearchComponent},
+  { path: 'student', canActivate: [AuthenticationGuard], component: StudentComponent},
+  { path: 'student_search', canActivate: [AuthenticationGuard], component: StudentSearchComponent},
+  { path: 'exam', canActivate: [AuthenticationGuard], component: ExamComponent},
+  { path: 'exam_search', canActivate: [AuthenticationGuard], component: ExamSearchComponent},
+  { path: 'fees', canActivate: [AuthenticationGuard], component: FeesComponent},
+  { path: 'fees_search', canActivate: [AuthenticationGuard], component: FeesSearchComponent},
+  { path: 'fees_setup', canActivate: [AuthenticationGuard], component: FeesSetupComponent},
+  { path: 'fees_head_search', canActivate: [AuthenticationGuard], component: FeesHeadSearchComponent},
+
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,30 @@ import { InstituteComponent } from './institute/institute.component';
 import { InstituteSearchComponent } from './institute-search/institute-search.component';
 import { HeaderComponent } from './header/header.component';
 import { LoginComponent } from './login/login.component';
+import { FooterComponent } from './footer/footer.component';
+import { TrustComponent } from './trust/trust.component';
+import { TrusteeComponent } from './trustee/trustee.component';
 import {AppRoutingModule} from './app-routing.module';
 import {AuthenticationGuard} from "./authentication.guard";
 import {AuthenticationService} from "./authentication.service";
 import { HttpClientModule } from '@angular/common/http';
 import { HttpModule } from '@angular/http';
+import { DesignationComponent } from './designation/designation.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { EmployeeSearchComponent } from './employee-search/employee-search.component';
+import { StandardComponent } from './standard/standard.component';
+import { StandardSearchComponent } from './standard-search/standard-search.component';
+import { SubjectComponent } from './subject/subject.component';
+import { SubjectSearchComponent } from './subject-search/subject-search.component';
+import { StudentComponent } from './student/student.component';
+import { StudentSearchComponent } from './student-search/student-search.component';
+import { ExamComponent } from './exam/exam.component';
+import { ExamSearchComponent } from './exam-search/exam-search.component';
+import { FeesComponent } from './fees/fees.component';
+import { FeesSearchComponent } from './fees-search/fees-search.component';
+import { FeesSetupComponent } from './fees-setup/fees-setup.component';
+import { SearchFeesHeadComponent } from './search-fees-head/search-fees-head.component';
+import { FeesHeadSearchComponent } from './fees-head-search/fees-head-search.component';
 
 
 
@@ -23,6 +42,24 @@ import { HttpModule } from '@angular/http';
     HeaderComponent,
     InstituteComponent,
     LoginComponent,
+    FooterComponent,
+    TrustComponent,
+    TrusteeComponent,
+    DesignationComponent,
+    EmployeeComponent,
+    EmployeeSearchComponent,
+    StandardComponent,
+    StandardSearchComponent,
+    SubjectComponent,
+    SubjectSearchComponent,
+    StudentComponent,
+    StudentSearchComponent,
+    ExamComponent,
+    ExamSearchComponent,
+    FeesComponent,
+    FeesSearchComponent,
+    FeesSetupComponent,
+    FeesHeadSearchComponent,
   ],
   imports: [
     BrowserModule,
